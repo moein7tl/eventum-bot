b608622c223e012b66a6fb14c342f02277b48a40
moein7tl <moein7tl@moein>
2014-08-21 16:33:14 +0430

    change firstOption to url

--- modules/broker.js
+++ modules/broker.js
@@ -1,25 +1,15 @@
 var amqp               =   require('amqp'),brokerConnection,exchange,queue;
 module.exports  =   function(_C,undefined){    
     var firstOption         =   {
-            host:                       _C.AMPQ_HOST,
-            port:                       _C.AMPQ_PORT,
-            login:                      _C.AMPQ_USER,
-            password:                   _C.AMPQ_PASSWORD,
-            connectionTimeout:          0,
-            authMechanism:              'AMQPLAIN',
-            vhost:                      '/',
-            noDelay:                    true,
-            ssl: { 
-                    enabled : false
-                }
-            },            
+        url:    _C.AMPQ_URL
+        },            
         
         secondOption        =   {
             reconnect:                  _C.AMPQ_RECONNECT,
             reconnectBackoffStrategy:   _C.AMPQ_RECONNECT_STRATEGY,
             reconnectBackoffTime:       _C.AMPQ_BACKOFF_TIME,
         };
-        
+
     if (!brokerConnection) brokerConnection    =   amqp.createConnection(firstOption,secondOption);
     
     brokerConnection.on('ready',function(){
@@ -29,8 +19,7 @@ module.exports  =   function(_C,undefined){
             queue   =   q;
         });
     });
-    
-    
+        
     function publish(msg){
         if (exchange){
             exchange.publish(_C.RABBITMQ_QUEUE, msg,{deliveryMode: 2}); 
